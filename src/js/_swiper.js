3f2032c1147a1d9e6b8017616267e2338a1f0467
Alim <alim-halilko@mail.ru>
2021-10-25 16:35:31 +0300

    добавлены страницы публикаций и проектов

--- src/js/_swiper.js
+++ src/js/_swiper.js
@@ -15,11 +15,29 @@ let swiper = new Swiper(".fullPage__swiper", {
 let swiperPagination = document.querySelector(".fullPage__pagination");
 
 swiper.on("slideChange", function(e) { // на событие смены слайда скрываем пагинацию на первом (нулевой индекс) слайде
-    console.log(e.activeIndex);
+    // console.log(e.activeIndex);
     if (e.activeIndex === 0) {
         swiperPagination.classList.add("fullPage__pagination--hidden");
     }
     else {
         swiperPagination.classList.remove("fullPage__pagination--hidden");
     }
+});
+
+let swiperProject = new Swiper(".projects__swiper", {
+    slidesPerView: 2,
+    spaceBetween: 0,
+    navigation: {
+        nextEl: ".projects__pagination .swiper-button-next",
+        prevEl: ".projects__pagination .swiper-button-prev",
+    },
+});
+
+let swiperPublications = new Swiper(".publications__swiper", {
+    slidesPerView: 3,
+    spaceBetween: 75,
+    navigation: {
+        nextEl: ".publications__pagination .swiper-button-next",
+        prevEl: ".publications__pagination .swiper-button-prev",
+    },
 });
